160c65869c46ac952d63b67211464f295bf661e8
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 17:11:17 -0500

    Update 34-primer-y-ultimo.js

--- 34-primer-y-ultimo.js
+++ 34-primer-y-ultimo.js
@@ -2,4 +2,13 @@ function primerYultimo(arr) {
     return [arr[0],arr[arr.length - 1]]
 }
 
-console.log(primerYultimo([5,6,4,2,3,5,"sdf"]))
+console.log(primerYultimo([5,6,4,2,3,5,"sdf"]));
+
+// Codigo del curso:
+function primerYultimo(elementos) {
+  let nuevosElementos = [];
+  nuevosElementos.push(elementos[0], elementos[elementos.length - 1]);
+  return nuevosElementos;
+}
+
+console.log(primerYultimo([100, 200, 300, 700]));
