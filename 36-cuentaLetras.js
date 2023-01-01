75faa8ce032abe5b433cea77d30c9ffefdcc2633
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 18:01:56 -0500

    Update 36-cuentaLetras.js

--- 36-cuentaLetras.js
+++ 36-cuentaLetras.js
@@ -13,4 +13,28 @@ function cuentaLetras(string) {
     consonates: ${consonates} `
 }
 
-console.log(cuentaLetras("Aáéíóóígghjgo"))
+console.log(cuentaLetras("Aáéíóóígghjgo"));
+
+// Codigo del curso:
+function cuentaLetras(texto) {
+  let consonantes = 0, vocales = 0, texto_limpio = '';
+  texto_limpio = texto
+    .split('')
+    .filter(letra => /[áéíóú\w]/gi.test(letra) && isNaN(letra))
+    .join('');
+
+  for (let letra of texto_limpio) {
+    if (/[áéíóúaeiou]/gi.test(letra)) {
+      vocales++;
+    } else {
+      consonantes++;
+    }
+  }
+
+  return [consonantes, vocales];
+}
+
+let letras = cuentaLetras('Hola !! . , - que tal á 1 89fd');
+
+console.log('Consonantes:', letras[0]);
+console.log('Vocales:', letras[1]);
