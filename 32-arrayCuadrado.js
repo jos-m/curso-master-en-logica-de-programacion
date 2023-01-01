2f143f2f56f3b70eb9cacf4527d243822d52a15a
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 15:05:10 -0500

    Update 32-arrayCuadrado.js

--- 32-arrayCuadrado.js
+++ 32-arrayCuadrado.js
@@ -2,4 +2,15 @@ function arrayCuadrado(arr){
     return arr.filter(el=> typeof(el)==="number")
               .map(el => el*el);
 }
-console.log(arrayCuadrado(["hola",2,6,7,9,90]))
+console.log(arrayCuadrado(["hola",2,6,7,9,90]));
+
+// Codigo del curso:
+function alCuadrado(numeros) {
+  let numeros_cuadrados = numeros
+    .filter(el => typeof el == 'number')
+    .map(el => Math.pow(el, 2));
+
+  return numeros_cuadrados;
+}
+
+console.log(alCuadrado([3, 4, 5, 'hola', true, ['hola']]));
