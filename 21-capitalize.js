0ba08abd7a15fcebcc4931faa8f395cf81a4f372
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2022-12-31 22:28:49 -0500

    🙂

--- 21-capitalize.js
+++ 21-capitalize.js
@@ -8,3 +8,20 @@ function capitalize(str){
 	
 };
 console.log(capitalize("hola mundo del ssd con ?kwl"));
+
+
+// Codigo del curso:
+function recortar(texto, hasta) {
+  let resultado = '';
+
+  if (typeof texto === 'string' && typeof hasta === 'number') {
+    resultado = texto.substring(0, hasta);
+  } else {
+    resultado = 'Introduce bien los datos!!';
+  }
+  return resultado;
+}
+
+console.log(
+  recortar('Hello World Perú!!!', 5)
+)
