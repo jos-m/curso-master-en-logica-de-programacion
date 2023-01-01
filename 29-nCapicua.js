c312b4aa6289bbf09595da6ae7861a9f32ecf780
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 12:21:28 -0500

    Update 29-nCapicua.js

--- 29-nCapicua.js
+++ 29-nCapicua.js
@@ -7,4 +7,18 @@ function nCapicua(n){
     else return false;
 
 }
-console.log(nCapicua(2002))
+console.log(nCapicua(2002));
+
+// Codigo del curso:
+
+function capi(numero) {
+  let procesado = parseInt(numero
+    .toString()
+    .split('')
+    .reverse()
+    .join('')
+  );
+  return numero === procesado;
+}
+
+console.log(capi(2002));
