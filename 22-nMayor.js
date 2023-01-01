4b85b8e8291eb23e22d8da480ea4cac75e586998
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2022-12-31 22:24:50 -0500

    ☺

--- 22-nMayor.js
+++ 22-nMayor.js
@@ -2,4 +2,26 @@ function nMayor(num,num2){
 	let result = (num>num2)?`${num} es mayor ${num2}`:(num<num2)?`${num2} es mayor ${num}`:(num2===num)?`iguales`:`no son numeros`
 	return result;
 }
-console.log(nMayor(2,2.5))
+console.log(nMayor(2,2.5));
+
+
+// Codigo del curso:
+function mayorMenor(numero1, numero2) {
+  let resultado = "";
+
+  if (numero1 === numero2) {
+    resultado = "Los numeros son iguales !!";
+  } else if (numero1 > numero2) {
+    resultado = "El numero mayor es: " + numero1 + "\n";
+    resultado += "El numero menor es: " + numero2;
+  } else if (numero1 < numero2) {
+    resultado = "El numero mayor es:" + numero2 + "\n";
+    resultado += "El numero menor es:" + numero1;
+  } else {
+    resultado = "Introduce numeros correctos";
+  }
+
+  return resultado;
+}
+
+console.log(mayorMenor(99, 180));
