0aefb863e3237c4eef4ac9ac16603a08745ec1f7
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 16:16:29 -0500

    Update 33-horaConSetInterval.js

--- 33-horaConSetInterval.js
+++ 33-horaConSetInterval.js
@@ -29,4 +29,50 @@ class Hora {
 }
 a = new Hora;
 a.encender();
-//a.actualizar()
+//a.actualizar();
+
+// Codigo del curso:
+class Reloj {
+
+  constructor() {
+    this.fecha = new Date();
+    this.horas = this.fecha.getHours();
+    this.minutos = this.fecha.getMinutes();
+    this.segundos = this.fecha.getSeconds();
+  }
+
+  actualizar(segundos) {
+    this.segundos += segundos;
+
+    // Actualizar segundos
+    if (this.segundos >= 60) {
+      this.minutos++;
+      this.segundos = 0;
+    }
+
+    // Actualizar minutos
+    if (this.minutos >= 60) { 
+      this.horas++;
+      this.minutos = 0;
+    }
+
+    // Actualizar horas
+    if (this.horas >= 24) {
+      this.horas = 0;
+    }
+  }
+
+  mostrar() {
+    this.actualizar(1);
+    console.log(`${this.horas}:${this.minutos}:${this.segundos}`);
+  }
+
+  encender() {
+    setInterval(() => {
+      this.mostrar();
+    }, 1000);
+  }
+}
+
+let mi_reloj = new Reloj();
+mi_reloj.encender();
