5596c81e4031e75fc65ef387f9a05b92cfd5d393
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 12:07:09 -0500

    Update 28-factorial.js

--- 28-factorial.js
+++ 28-factorial.js
@@ -5,4 +5,17 @@ function factorial(num){
     return num * factorial(num-1)
 }
 
-console.log(factorial(5))
+console.log(factorial(5));
+
+// Codigo del curso:
+function factorial(numero) {
+  let resultado = 1;
+
+  for (let i = 0; i <= numero; i++) {
+    resultado *= i;
+  }
+
+  return resultado;
+}
+
+console.log('El resultado de 3 es: ', factorial(3));
