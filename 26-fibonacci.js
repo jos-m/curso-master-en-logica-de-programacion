e9fa98d57b085c0157805ec7a2ec80e3fba54c0c
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 10:45:54 -0500

    Update 26-fibonacci.js

--- 26-fibonacci.js
+++ 26-fibonacci.js
@@ -15,4 +15,17 @@ function nFibonacci(n) {
   
   
   }
-  console.log(nFibonacci(8))
+  console.log(nFibonacci(8));
+
+// Codigo del curso:
+function fibonacci(numero) {
+  let serie = [0, 1];
+
+  for (let i = 2; i <= numero; i++) {
+    serie.push(serie[i - 1] + serie[i - 2]);
+  }
+  return [serie, serie[numero]];
+}
+
+console.log('Serie completa: ',fibonacci(10)[0]);
+console.log('Resultado final: ',fibonacci(10)[1]);
