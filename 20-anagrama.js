90dcff16a3dd3ddb2ed25130198082451ee5b8af
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2022-12-31 20:57:29 -0500

    😃

--- 20-anagrama.js
+++ 20-anagrama.js
@@ -6,3 +6,21 @@ function cleaner(str) {
 }
 
 console.log(anagramas("ser?gio", "riesgo"));
+
+// Codigo del curso :
+function limpiarTexto(texto) {
+  return texto
+    .replace(/[^\w]/gi, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
+
+function anagramas(texto1, texto2) {
+  return limpiarTexto(texto1) === limpiarTexto(texto2);
+}
+
+console.log(limpiarTexto('SerGio !! -.,'));
+
+console.log(anagramas('SerGio !! -.,', ' sergIO???;:"'));
