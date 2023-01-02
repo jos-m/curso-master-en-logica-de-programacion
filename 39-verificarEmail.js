a6f34f168309cb1132001615d2546d031f329b98
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 21:32:52 -0500

    Update 39-verificarEmail.js

--- 39-verificarEmail.js
+++ 39-verificarEmail.js
@@ -1,3 +1,10 @@
 function verificarEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/.test(email)
 }
+
+// Codigo del curso:
+function comprobarEmail(email){
+  return /^\w+@\w+\.\w+$/gi.test(email);
+}
+
+console.log(comprobarEmail('correo@gmail.com'));
