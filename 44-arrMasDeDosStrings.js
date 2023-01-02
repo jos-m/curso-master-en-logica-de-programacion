03c355d6e1d9b4651f7126897e2521fecbbf7947
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-02 08:13:03 -0500

    Update 44-arrMasDeDosStrings.js

--- 44-arrMasDeDosStrings.js
+++ 44-arrMasDeDosStrings.js
@@ -5,4 +5,18 @@ function masDeDosStrings(arr) {
         }
     })
 }
-console.log(masDeDosStrings(["hola asdas", "asds", "asdassdfsdsdf", " "]))
+console.log(masDeDosStrings(["hola asdas", "asds", "asdassdfsdsdf", " "]));
+
+// Codigo del curso:
+function dosPalabras(elementos) {
+  let nuevos_elementos = [];
+  for (palabra of elementos) {
+    if (palabra.split(' ').length >= 2) {
+      nuevos_elementos.push(palabra);
+    }
+  }
+
+  return nuevos_elementos;
+}
+
+console.log(dosPalabras(['hola', 'hola que tal', 'san francisco', 'ciudad real', 'victor']));
