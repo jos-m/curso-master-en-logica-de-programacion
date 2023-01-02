8172138de3371bb1d638a2e35add83f3759e2446
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 21:40:51 -0500

    Update 40-valorMasBajoY_Alto.js

--- 40-valorMasBajoY_Alto.js
+++ 40-valorMasBajoY_Alto.js
@@ -4,4 +4,16 @@ function valorAltoY_bajo(arr) {
     valor mas alto: ${valores[valores.length-1]}
     valor mas bajo: ${valores[0]} `
 }
-console.log(valorAltoY_bajo([1,2,3,6,5,2]))
+console.log(valorAltoY_bajo([1,2,3,6,5,2]));
+
+// Codigo del curso:
+function altoBajo(numeros) {
+  let ordenados = numeros.sort((a, b) => a - b);
+
+  return {
+    bajo: ordenados[0],
+    alto: ordenados[ordenados.length - 1]
+  }
+}
+
+console.log(altoBajo([88, 12, 41, 31, 74, 95, 63]));
