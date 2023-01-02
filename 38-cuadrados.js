2f244f9a6ed03ec582eb31158a1fc521ca196712
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 21:24:39 -0500

    Update 38-cuadrados.js

--- 38-cuadrados.js
+++ 38-cuadrados.js
@@ -1,6 +1,19 @@
 function cuadrados(num) {
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i <= num; i++) {
         console.log(Math.pow(i,2))
     }
 }
-cuadrados(5)
+cuadrados(5);
+
+// Codigo del curso:
+function cuadrado(numero){
+  return numero * numero;
+}
+
+function mostrar(numero){
+  for (let i = 0; i <= numero; i++) {
+    console.log(cuadrado(i))
+  }
+}
+
+mostrar(7);
