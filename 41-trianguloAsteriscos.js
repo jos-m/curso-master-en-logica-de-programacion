457cdae58527475a4c3bf1b4a36d051983a12a95
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-01 22:49:06 -0500

    Update 41-trianguloAsteriscos.js

--- 41-trianguloAsteriscos.js
+++ 41-trianguloAsteriscos.js
@@ -8,5 +8,30 @@ function trinanguloAsteriscos(num) {
     }
     return triangle;
 }
-console.log(trinanguloAsteriscos(40))
+console.log(trinanguloAsteriscos(4));
+
+// Codigo del curso:
+function triangulo(numero) {
+  let mitad = Math.floor(numero - 1);
+  let resultado = '';
+
+  // bucle total filas
+  for (let fila = 0; fila < numero; fila++) {
+    let nivel = '';
+
+    // bucle para pintar asteriscos y espacios
+    for (let columna = 0; columna < (2 * numero - 1); columna++) {
+      if (mitad - fila <= columna && mitad + fila >= columna) {
+        nivel += '*';
+      } else {
+        nivel += ' ';
+      }
+    }
+
+    resultado += nivel + '\n';
+  }
+  return resultado;
+}
+
+console.log(triangulo(4));
 
