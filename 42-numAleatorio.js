ff1745ae96afebc38741d5d29387b26a2da8ced5
zaruk21 <83556926+zaruk21@users.noreply.github.com>
2023-01-02 07:19:16 -0500

    Update 42-numAleatorio.js

--- 42-numAleatorio.js
+++ 42-numAleatorio.js
@@ -2,3 +2,8 @@ function numAleatorio(min,max) {
     return Math.round(Math.random() * (max-min) + min)
 }
 console.log(numAleatorio(2,6))
+
+// ciclo for para generarlos:
+for (let i = 0; i < 10; i++) {
+  console.log(aleatorio(2, 6));
+}
